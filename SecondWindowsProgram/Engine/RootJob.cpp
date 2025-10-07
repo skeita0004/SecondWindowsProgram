40dbaff28a13e3270896818c9f687e8ca46d8b7e
啓大 齋藤 <240004@jc-21.jp>
2025-10-07 15:07:57 +0900

    RootJob追加

---
+++ SecondWindowsProgram/Engine/RootJob.cpp
@@ -0,0 +1,27 @@
+﻿#include "RootJob.h"
+#include "../Game/PlayScene.h"
+
+RootJob::RootJob()
+{
+}
+
+RootJob::~RootJob()
+{
+}
+
+void RootJob::Init()
+{
+	Instantiate<PlayScene> (this);
+}
+
+void RootJob::Update()
+{
+}
+
+void RootJob::Draw()
+{
+}
+
+void RootJob::Release()
+{
+}
---
+++ SecondWindowsProgram/Engine/RootJob.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "GameObject.h"
+
+class RootJob : public GameObject
+{
+public:
+	RootJob();
+	~RootJob();
+
+	void Init()    override;
+	void Update()  override;
+	void Draw()    override;
+	void Release() override;
+};
