d49e7d07e60813bdedd8da28090847a2465deec4
啓大 齋藤 <240004@jc-21.jp>
2025-10-07 15:07:42 +0900

    GameObject追加

---
+++ SecondWindowsProgram/Engine/GameObject.cpp
@@ -0,0 +1,74 @@
+﻿#include "GameObject.h"
+
+GameObject::GameObject() :
+	childList_(0, nullptr),
+	transForm_(),
+	pParent_(nullptr),
+	objectName_("Unknown Object"),
+	isKillMe_(false)
+{
+}
+
+GameObject::GameObject(GameObject* _pParent, const std::string& _name) :
+	childList_(0, nullptr),
+	transForm_(),
+	pParent_(_pParent),
+	objectName_(_name)
+{
+}
+
+GameObject::~GameObject()
+{
+}
+
+void GameObject::UpdateSub()
+{
+	Update();
+	
+	for (auto& object : childList_)
+	{
+		object->UpdateSub();
+	
+	}
+
+	for (auto itr = childList_.begin(); itr != childList_.end();)
+	{
+		auto object = *itr;
+
+		if (object->isKillMe_)
+		{
+			object->ReleaseSub();
+			SafeDelete(object);
+			itr = childList_.erase(itr);
+		}
+		else
+		{
+			itr++;
+		}
+	}
+}
+
+void GameObject::DrawSub()
+{
+	Draw();
+
+	for (auto& object : this->childList_)
+	{
+		object->DrawSub();
+	}
+}
+
+void GameObject::ReleaseSub()
+{
+	for (auto& object : this->childList_)
+	{
+		object->ReleaseSub();
+	}
+
+	Release(); // 自分を先にリリースすると破滅するので、最後に呼ぶ。
+}
+
+void GameObject::KillMe()
+{
+	isKillMe_ = true;
+}
---
+++ SecondWindowsProgram/Engine/GameObject.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <list>
+#include <string>
+#include "Transform.h"
+
+class GameObject
+{
+public:
+	GameObject();
+	GameObject(GameObject* _pParent, const std::string& _name);
+	virtual ~GameObject();
+
+	virtual void Init()    = 0;
+	virtual void Update()  = 0;
+	virtual void Draw()    = 0;
+	virtual void Release() = 0;
+
+	void UpdateSub();
+	void DrawSub();
+	void ReleaseSub();
+
+	void KillMe();
+
+	template <typename PTYPE>
+	inline static void SafeDelete(PTYPE* _ptr)
+	{
+		if (_ptr != nullptr)
+		{
+			delete _ptr;
+			_ptr = nullptr;
+		}
+	}
+
+	template <class T>
+	inline void Instantiate(GameObject* _parent)
+	{
+		T* pObject{nullptr};
+		pObject = new T(_parent);
+		pObject->Init();
+		_parent->childList_.push_back(pObject);
+	}
+
+private:
+	std::list<GameObject*> childList_;
+	Transform transForm_;
+	GameObject* pParent_;
+	std::string objectName_;
+	bool isKillMe_;
+};
