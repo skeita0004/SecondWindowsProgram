545d3be9f0d3994fb4baa2b370b6afb268c8b0af
啓大 齋藤 <240004@jc-21.jp>
2025-10-07 15:11:40 +0900

    プレイシーン追加

---
+++ SecondWindowsProgram/Game/PlayScene.cpp
@@ -0,0 +1,29 @@
+﻿#include "PlayScene.h"
+#include "Player.h"
+
+PlayScene::PlayScene(GameObject* _parent) :
+	GameObject(_parent, "PlayScene")
+{
+}
+
+PlayScene::~PlayScene()
+{
+}
+
+void PlayScene::Init()
+{
+	Instantiate<Player>(this);
+}
+
+void PlayScene::Update()
+{
+}
+
+void PlayScene::Draw()
+{
+	
+}
+
+void PlayScene::Release()
+{
+}
---
+++ SecondWindowsProgram/Game/PlayScene.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "../Engine/GameObject.h"
+
+class PlayScene : public GameObject
+{
+public:
+	PlayScene(GameObject* _parent);
+	~PlayScene();
+
+	void Init()    override;
+	void Update()  override;
+	void Draw()    override;
+	void Release() override;
+};
