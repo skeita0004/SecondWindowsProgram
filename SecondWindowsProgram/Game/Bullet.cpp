80c8df377af03fe9e54a4d07901edc067d9b92ec
啓大 齋藤 <240004@jc-21.jp>
2025-10-07 15:12:07 +0900

    Bullet追加

---
+++ SecondWindowsProgram/Game/Bullet.cpp
@@ -0,0 +1,50 @@
+﻿#include "Bullet.h"
+#include "Player.h"
+#include <string>
+
+namespace
+{
+	const std::string MODEL_PATH{"Assets/models/mochi_kinchaku.fbx"};
+}
+
+Bullet::Bullet(GameObject* _parent):
+	GameObject(_parent, "Bullet"),
+	transform_(),
+	pModel_(nullptr)
+{
+}
+
+Bullet::~Bullet()
+{
+}
+
+void Bullet::Init()
+{
+	pModel_ = new Fbx();
+	pModel_->Load(MODEL_PATH);
+}
+
+void Bullet::Update()
+{
+	static int deadCounter = 0;
+
+	transform_.rotate.y = 90;
+	transform_.position.z--;
+
+	if (deadCounter >= 60 * 3)
+	{
+		KillMe();
+	}
+
+	deadCounter++;
+}
+
+void Bullet::Draw()
+{
+	pModel_->Draw(transform_);
+}
+
+void Bullet::Release()
+{
+	SAFE_RELEASE(pModel_);
+}
---
+++ SecondWindowsProgram/Game/Bullet.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "GameObject.h"
+#include "Fbx.h"
+
+class Bullet : public GameObject
+{
+public:
+	Bullet(GameObject* _parent);
+	~Bullet();
+
+	void Init()    override;
+	void Update()  override;
+	void Draw()    override;
+	void Release() override;
+
+private:
+	Transform transform_;
+	Fbx* pModel_;
+};
