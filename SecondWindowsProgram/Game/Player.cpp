c0045faf5d47f2dacf176985e81eddc2053a37e3
啓大 齋藤 <240004@jc-21.jp>
2025-10-07 15:11:52 +0900

    プレイヤー追加

---
+++ SecondWindowsProgram/Game/Player.cpp
@@ -0,0 +1,55 @@
+﻿#include "Player.h"
+#include <string>
+#include "Input.h"
+#include "Bullet.h"
+
+namespace
+{
+	const std::string MODEL_PATH{"Assets/models/oden.fbx"};
+}
+
+Player::Player(GameObject* _parent) :
+	GameObject(_parent, "Player"),
+	pModel_(nullptr),
+	transform_()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Init()
+{
+	pModel_ = new Fbx();
+	pModel_->Load(MODEL_PATH);
+}
+
+void Player::Update()
+{
+	transform_.rotate.y--;
+
+	if (Input::IsKeyDown(DIK_SPACE))
+	{
+		Instantiate<Bullet>(this);
+	}
+
+	if (Input::IsKey(DIK_A))
+	{
+		transform_.position.x -= 0.2f;
+	}
+	if (Input::IsKey(DIK_D))
+	{
+		transform_.position.x += 0.2f;
+	}
+}
+
+void Player::Draw()
+{
+	pModel_->Draw(transform_);
+}
+
+void Player::Release()
+{
+	pModel_->Release();
+}
---
+++ SecondWindowsProgram/Game/Player.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "GameObject.h"
+#include "Fbx.h"
+
+class Player : public GameObject
+{
+public:
+	Player(GameObject* _parent);
+	~Player();
+
+	void Init()    override;
+	void Update()  override;
+	void Draw()    override;
+	void Release() override;
+
+private:
+	Fbx* pModel_;
+	Transform transform_;
+};
