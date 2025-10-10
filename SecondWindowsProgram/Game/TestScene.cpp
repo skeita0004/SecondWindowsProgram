4fd557ca20e04f47a3701991d598654e85ff5d9f
啓大 齋藤 <240004@jc-21.jp>
2025-10-10 14:29:16 +0900

    TestScene追加

---
+++ SecondWindowsProgram/Game/TestScene.cpp
@@ -0,0 +1,32 @@
+﻿#include "TestScene.h"
+#include "Input.h"
+
+TestScene::TestScene(GameObject* _parent) :
+	GameObject(_parent, "TestScene")
+{
+}
+
+TestScene::~TestScene()
+{
+}
+
+void TestScene::Init()
+{
+	pSceneManager = FindGameObject<SceneManager>("SceneManager");
+}
+
+void TestScene::Update()
+{
+	if (Input::IsKey(DIK_P))
+	{
+		pSceneManager->ChangeScene(Scene::SceneID::SID_PLAY);
+	}
+}
+
+void TestScene::Draw()
+{
+}
+
+void TestScene::Release()
+{
+}
---
+++ SecondWindowsProgram/Game/TestScene.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "GameObject.h"
+#include "SceneManager.h"
+
+class TestScene : public GameObject
+{
+public:
+	TestScene(GameObject* _parent);
+	~TestScene();
+
+	void Init()    override;
+	void Update()  override;
+	void Draw()    override;
+	void Release() override;
+
+private:
+	SceneManager* pSceneManager;
+};
