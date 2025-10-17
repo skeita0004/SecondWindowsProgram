eb762b608c6f727070d2f52876f66167ed957042
啓大 齋藤 <240004@jc-21.jp>
2025-10-17 18:21:44 +0900

    Modelクラスのフライウェイトをちゃんとした形に

--- SecondWindowsProgram/Engine/Model.cpp
+++ SecondWindowsProgram/Engine/Model.cpp
@@ -1,13 +1,22 @@
 ﻿#include "Model.h"
+#undef FindResource
 
 namespace
 {
-    std::vector<Model::Model> modelList;
+    enum MODEL_STATE
+    {
+        NOTFOUND = -1
+    };
+
+    //std::vector<Model::Model> modelList;
+
+    std::vector<Model::Resource> resourseList;
+    std::vector<Model::Instance> instanceList;
 
     bool IsDuplicate(const std::string& _path, int* _index)
     {
         int i = 0;
-        for (auto& model : modelList)
+        for (auto& model : resourseList)
         {
             i++;
             if (model.fileName == _path)
@@ -18,50 +27,100 @@ namespace
         }
         return false;
     }
+
+    int FindResource(const std::string& _path)
+    {
+        for (int i = 0; i < resourseList.size(); i++)
+        {
+            if (resourseList[i].fileName == _path)
+            {
+                return i;
+            }
+        }
+        return NOTFOUND;
+    }
 }
 
 int Model::Load(const std::string& _path)
 {
     int index{0};
 
-    if (not(IsDuplicate(_path, &index)))
+    // 重複しなければ、新規登録
+    //if (not(IsDuplicate(_path, &index)))
+    //{
+    //    Model model
+    //    {
+    //        .pModel{nullptr},
+    //        .fileName{_path}
+    //    };
+
+    //    Resource modelRes
+    //    {
+    //        .pModel{nullptr},
+    //        .fileName{_path}
+    //    };
+
+    //    model.pModel = new Fbx();
+    //    model.pModel->Load(model.fileName);
+
+    //    modelList.push_back(model);
+    //    index = { static_cast<int>(modelList.size() - 1) };
+    //    return index;
+    //}
+
+    int resIndex = FindResource(_path);
+
+    if (resIndex == -1)
     {
-        Model model
-        {
-            .pModel{nullptr},
-            .fileName{_path}
-        };
-
-        model.pModel = new Fbx();
-        model.pModel->Load(model.fileName);
-
-        modelList.push_back(model);
-        index = { static_cast<int>(modelList.size() - 1) };
-        return index;
+        Resource modelRes{};
+        modelRes.pModel = new Fbx();
+        modelRes.pModel->Load(_path);
+        modelRes.fileName = _path;
+        resourseList.push_back(modelRes);
+        resIndex = static_cast<int>(resourseList.size() - 1);
     }
 
-    return index;
+    Instance modelIns{};
+    modelIns.resourseIndex = resIndex;
+    modelIns.transform = {};
+    instanceList.push_back(modelIns);
+
+    return static_cast<int>(instanceList.size() - 1);
 }
 
 void Model::Draw(int _modelHandle)
 {
-    for (auto& model : modelList)
-    {
-        model.pModel->Draw(model.transform);
-    }
+    //for (auto& model : modelList)
+    //{
+    //    model.pModel->Draw(model.transform);
+    //}
+
+    auto& modelIns = instanceList[_modelHandle];
+    auto& modelRes = resourseList[modelIns.resourseIndex];
+
+    modelRes.pModel->Draw(modelIns.transform);
 }
 
-void Model::SetTransForm(int _modelHandle, Transform _transform)
+void Model::SetTransForm(int _modelHandle, const Transform& _transform)
 {
-    modelList[_modelHandle].transform = _transform;
+    instanceList[_modelHandle].transform = _transform;
 }
 
 void Model::AllRelease()
 {
-    for (auto itr = modelList.begin(); itr != modelList.end();)
+    //for (auto itr = modelList.begin(); itr != modelList.end();)
+    //{
+    //    auto model = (*itr);
+    //    SAFE_RELEASE(model.pModel);
+    //    itr = modelList.erase(itr);
+    //}
+
+    for (auto itr = resourseList.begin(); itr != resourseList.end();)
     {
         auto model = (*itr);
         SAFE_RELEASE(model.pModel);
-        itr = modelList.erase(itr);
+        itr = resourseList.erase(itr);
     }
+    resourseList.clear();
+    instanceList.clear();
 }
--- SecondWindowsProgram/Engine/Model.h
+++ SecondWindowsProgram/Engine/Model.h
@@ -7,17 +7,27 @@
 
 namespace Model
 {
-	struct Model
-	{
-		Fbx* pModel;
-		std::string fileName;
-		Transform transform;
-	};
+    struct Resource
+    {
+        Fbx* pModel;
+        std::string fileName;
+    };
 
+    struct Instance
+    {
+        int resourseIndex;
+        Transform transform;
+    };
 
+	//struct Model
+	//{
+	//	Fbx* pModel;
+	//	std::string fileName;
+	//	Transform transform;
+	//};
 
 	int  Load(const std::string& _path);
 	void Draw(int _modelHandle);
-	void SetTransForm(int _modelHandle, Transform _transform);
+	void SetTransForm(int _modelHandle, const Transform& _transform);
 	void AllRelease();
 }
