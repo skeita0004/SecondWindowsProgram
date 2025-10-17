4926ad34f12820f6db641d1518e9c5719c2954ad
啓大 齋藤 <240004@jc-21.jp>
2025-10-17 18:18:52 +0900

    Orbiter追加

---
+++ SecondWindowsProgram/Game/Orbiter.cpp
@@ -0,0 +1,47 @@
+﻿#include "Orbiter.h"
+#include <string>
+#include "Input.h"
+#include "Bullet.h"
+#include "Model.h"
+
+namespace
+{
+    const std::string MODEL_PATH{ "Assets/models/mochi_kinchaku.fbx" };
+}
+
+Orbiter::Orbiter(GameObject* _parent) :
+    GameObject(_parent, "Orbiter"),
+    hModel_(-1),
+    transform_(),
+    pSphereCollider_(nullptr)
+{
+}
+
+Orbiter::~Orbiter()
+{
+}
+
+void Orbiter::Init()
+{
+    // pSphereCollider_ = new SphereCollider(transform_.position, 1.f);
+    hModel_ = Model::Load(MODEL_PATH);
+    transform_.rotate.y = 90;
+    Model::SetTransForm(hModel_, transform_);
+}
+
+void Orbiter::Update()
+{
+    transform_.scale = {0.7, 0.7, 0.7};
+    transform_.rotate.y -= 10 / 60;
+
+    Model::SetTransForm(hModel_, transform_);
+}
+
+void Orbiter::Draw()
+{
+    Model::Draw(hModel_);
+}
+
+void Orbiter::Release()
+{
+}
---
+++ SecondWindowsProgram/Game/Orbiter.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "GameObject.h"
+
+class Orbiter : public GameObject
+{
+public:
+    Orbiter(GameObject* _parent);
+    ~Orbiter();
+
+    void Init()    override;
+    void Update()  override;
+    void Draw()    override;
+    void Release() override;
+
+private:
+    int       hModel_;
+    Transform transform_;
+    SphereCollider* pSphereCollider_;
+};
