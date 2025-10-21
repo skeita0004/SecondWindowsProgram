6aceeac5ae5974967ea0971ec91220d82b392c01
啓大 齋藤 <240004@jc-21.jp>
2025-10-21 18:38:42 +0900

    安全にポインタを開放する関数群を追加

---
+++ SecondWindowsProgram/Engine/SafeCleaning.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <concepts>
+
+namespace SafeCleaning
+{
+    template<typename TYPE>
+    concept HasRelease = requires(TYPE _ptr)
+    {
+        _ptr.Release();
+    };
+
+    template<typename TYPE>
+    inline static void SafeDelete(TYPE*& _ptr)
+    {
+        if (_ptr != nullptr)
+        {
+            delete _ptr;
+            _ptr = nullptr;
+        }
+    }
+
+    template<typename TYPE>
+    inline static void SafeDeleteArray(TYPE*& _ptr)
+    {
+        if (_ptr != nullptr)
+        {
+            delete[] _ptr;
+            _ptr = nullptr;
+        }
+    }
+
+    template<HasRelease TYPE>
+    inline static void SafeRelease(TYPE*& _ptr)
+    {
+        if (_ptr != nullptr)
+        {
+            _ptr->Release();
+            _ptr = nullptr;
+        }
+    }
+}
