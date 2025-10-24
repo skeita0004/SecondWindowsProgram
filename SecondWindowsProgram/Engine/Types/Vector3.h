4f93da321ead569077f067e83f50a81feacd1f43
啓大 齋藤 <240004@jc-21.jp>
2025-10-24 18:28:01 +0900

    使いやすいVector3追加中

---
+++ SecondWindowsProgram/Engine/Types/Vector3.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+//
+//#include <DirectXMath.h>
+//
+//struct Vector3 : public DirectX::XMFLOAT3
+//{
+//    Vector3 operator+(const Vector3& _rv)
+//    {
+//        Vector3 tmp;
+//        tmp.x = this->x + _rv.x;
+//        tmp.y = this->y + _rv.y;
+//        tmp.z = this->z + _rv.z;
+//    }
+//};
