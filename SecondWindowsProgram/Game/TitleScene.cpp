d7b3fa0fb8fca16e607a4d177c70ddd94379b94e
啓大 齋藤 <240004@jc-21.jp>
2025-11-14 18:35:00 +0900

    テストシーンの中身を丸ごとタイトルシーンへとコピー

--- SecondWindowsProgram/Game/TitleScene.cpp
+++ SecondWindowsProgram/Game/TitleScene.cpp
@@ -2,6 +2,8 @@
 #include "SceneManager.h"
 #include "Model.h"
 #include "Input.h"
+#include "TeaTime.h"
+#include "TitleLogo.h"
 
 TitleScene::TitleScene(GameObject* _parent) :
     GameObject(_parent, "TitleScene"),
@@ -16,17 +18,21 @@ TitleScene::~TitleScene()
 
 void TitleScene::Init()
 {
-
+    pSceneManager_ = FindGameObject<SceneManager>("SceneManager");
+    Instantiate<TeaTime>(this);
+    Instantiate<TitleLogo>(this);
 }
 
 void TitleScene::Update()
 {
-
+    if (Input::IsKeyDown(DIK_P))
+    {
+        pSceneManager_->ChangeScene(SceneManager::SceneID::SID_PLAY);
+    }
 }
 
 void TitleScene::Draw()
 {
-
 }
 
 void TitleScene::Release()
