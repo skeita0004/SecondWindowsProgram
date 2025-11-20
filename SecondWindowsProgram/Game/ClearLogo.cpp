7123fa12803090848baef6e3d8418c8315f4bb68
啓大 齋藤 <240004@jc-21.jp>
2025-11-20 15:20:52 +0900

    クリアシーンのロゴ

---
+++ SecondWindowsProgram/Game/ClearLogo.cpp
@@ -0,0 +1,41 @@
+﻿#include "ClearLogo.h"
+#include "Model.h"
+#include <timeapi.h>
+#include <algorithm>
+#undef max
+
+ClearLogo::ClearLogo(GameObject* _parent) :
+    GameObject(_parent, "ClearLogo"),
+    hModel_(-1)
+{
+}
+
+ClearLogo::~ClearLogo()
+{
+}
+
+void ClearLogo::Init()
+{
+    const float SCALE = 3.f;
+    hModel_ = Model::Load("Assets/models/clear_logo.fbx");
+    transform.position = XMFLOAT3(0.f, 50.f, 0.f);
+    transform.scale = XMFLOAT3(SCALE, SCALE, SCALE);
+}
+
+void ClearLogo::Update()
+{
+    if (transform.position.y >= 0.f)
+    {
+        transform.position.y -= 1.f;
+    }
+}
+
+void ClearLogo::Draw()
+{
+    Model::SetTransForm(hModel_, &transform);
+    Model::Draw(hModel_);
+}
+
+void ClearLogo::Release()
+{
+}
---
+++ SecondWindowsProgram/Game/ClearLogo.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class ClearLogo : public GameObject
+{
+public:
+    ClearLogo(GameObject* _parent);
+    ~ClearLogo();
+
+    void Init()    override;
+    void Update()  override;
+    void Draw()    override;
+    void Release() override;
+
+private:
+    int       hModel_;
+};
