78b2df4bfecf483e55bf12e814639b7f7c2e276e
啓大 齋藤 <saitou-keita0109@outlook.jp>
2025-11-25 00:19:05 +0900

    OverLogo追加

---
+++ SecondWindowsProgram/Game/OverLogo.cpp
@@ -0,0 +1,30 @@
+﻿#include "OverLogo.h"
+#include "Picture.h"
+
+OverLogo::OverLogo(GameObject* _parent) :
+    GameObject(_parent, "OverLogo")
+{
+}
+
+OverLogo::~OverLogo()
+{
+}
+
+void OverLogo::Init()
+{
+    hPicture_ = Picture::Load("Assets/overlogo.png");
+}
+
+void OverLogo::Update()
+{
+}
+
+void OverLogo::Draw()
+{
+    Picture::SetTransForm(hPicture_, &transform);
+    Picture::Draw(hPicture_);
+}
+
+void OverLogo::Release()
+{
+}
---
+++ SecondWindowsProgram/Game/OverLogo.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class OverLogo : public GameObject
+{
+public:
+    OverLogo(GameObject* _parent);
+    ~OverLogo();
+
+    void Init()    override;
+    void Update()  override;
+    void Draw()    override;
+    void Release() override;
+
+private:
+    int hPicture_;
+};
